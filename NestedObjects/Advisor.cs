c6227eb3b59d7d3bfd0e0b7bbce1f1595ce36eeb
William Owen <william.owen.bsc@gmail.com>
2025-01-20 15:30:33 -0800

    Added class files for Advisors and Students

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    internal class Advisor
+    {
+        /// <summary>
+        /// The legal first name of the advisor
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Email address of the advisor
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The office location of the advisor
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+
+
+    }
+}
